4e17d07132c72005a40451388564738cd79b3319
andrewdtest2 <112468731+andrewdtest2@users.noreply.github.com>
2022-11-09 18:39:39 +0800

    Update high.php

--- vulnerabilities/exec/source/high.php
+++ vulnerabilities/exec/source/high.php
@@ -18,7 +18,7 @@ if( isset( $_POST[ 'Submit' ]  ) ) {
 	);
 
 	// Remove any of the characters in the array (blacklist).
-	$target1 = str_replace( array_keys( $substitutions ), $substitutions, $target );
+	$target1 = escapeshellcmd(str_replace( array_keys( $substitutions ), $substitutions, $target ));
 
 	// Determine OS and execute the ping command.
 	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
